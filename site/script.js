4d90b042022cb1c584186bcdc472cedea5ee86b2
Vítor Viana <vitorviana@id.uff.br>
2024-04-16 13:32:04 -0300

    Botão veja mais de projeto

--- site/script.js
+++ site/script.js
@@ -3,3 +3,13 @@ function toggleMenu() {
     ul.classList.toggle('active');
 }
 
+document.getElementById("show-more-btn").addEventListener("click", function() {
+    var moreContent = document.getElementById("more-project-ul");
+    if (moreContent.style.display === "none") {
+        moreContent.style.display = "flex";
+        document.getElementById("show-more-btn").textContent = "Mostrar Menos";
+    } else {
+        moreContent.style.display = "none";
+        document.getElementById("show-more-btn").textContent = "Mostrar Mais";
+    }
+});
